63a4b80c5809b97eeb3c40ad2a15220a54010c4e
Niteesh <niteeshsingh5@gmail.com>
2020-02-27 17:20:35 +0530

    Correct changes

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,7 +1,7 @@
 const request = require('request')
 
 const forecast = (latitude, longitude, callback) => {
-    const url = 'https://api.darksky.net/forecast/7253cb79c9dc53eb2545a337027c0abb/' + latitude + ' , ' + longitude
+    const url = 'https://api.darksky.net/forecast/7253cb79c9dc53eb2545a337027c0abb/' + latitude + ',' + longitude
     
     request({ url, json: true }, (error, { body }) => {
         if(error) {
@@ -9,7 +9,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if(body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, `${response.body.daily.data[0].summary} It is currently ${response.body.currently.temperature} degree out. There is ${response.body.currently.precipProbability} % chance of rain. `)
+            callback(undefined, `${body.daily.data[0].summary} It is currently ${body.currently.temperature} degree out. There is ${body.currently.precipProbability} % chance of rain. `)
         }
     })
 }
